df6930734c53fe0a905211b31fffa245b1e6c723
miqueias <miqueiasricardobueno@gmail.com>
2026-07-17 15:04:40 -0300

    Soma de dois números inteiros

---
+++ capitulo3/exercicio_3_7.py
@@ -0,0 +1,5 @@
+# Faça um programa que peça dois números inteiros. Imprima a soma desses dois números na tela.
+num1 = int(input("Digite um número inteiro: "))
+num2 = int(input("Digite outro número inteiro: "))
+soma = num1 + num2
+print(f"A soma dos números {num1} + {num2} = {soma}")
