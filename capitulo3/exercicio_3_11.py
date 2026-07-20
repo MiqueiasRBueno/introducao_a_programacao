bdb6ed9d2ee4cb396b670fd282701589b0a750c1
miqueias <miqueiasricardobueno@gmail.com>
2026-07-20 09:00:22 -0300

    Calcula o valor de desconto ao cliente.

--- capitulo3/exercicio_3_11.py
+++ capitulo3/exercicio_3_11.py
@@ -1,2 +1,9 @@
 # Faça um programa que solicite o preço de uma mercadoria e o percentual de desconto.
 # Exiba o valor do desconto e o preço a pagar.
+
+valor_prod = round(float(input("Insira o valor do produto: R$ ")), 2)
+perc_desc = int(input("Insira a porcentagem a descontar: "))
+valor_desc = valor_prod * perc_desc / 100
+total_pg = valor_prod - valor_desc
+print(f'''Total do desconto: R${valor_desc:.2f}
+Total à pagar: R${total_pg:.2f}''')
