e9b5ef7dba5001491ec7a85a22602fd629f44dc6
miqueias <miqueiasricardobueno@gmail.com>
2026-07-20 11:42:28 -0300

    Calcula o tempo necessário para uma viagem.

--- capitulo3/exercicio_3_12.py
+++ capitulo3/exercicio_3_12.py
@@ -1,2 +1,10 @@
 # Escreva um programa que calcule o tempo de uma viagem de carro.
 # Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+
+dist_percorrer = round(float(input("Qual a distância a percorrer?: ")),2)
+vel_media = int(input("Qual a velocidade média estimada?: "))
+temp_total =  dist_percorrer / vel_media
+mint_total = (temp_total - int(temp_total)) * 60
+print("A uma velocidade média de %dkm/h" %vel_media)
+print("Você percorrerá a distância de %5.2fkm" %dist_percorrer)
+print(f"Em {int(temp_total)}h{int(mint_total)}min")
