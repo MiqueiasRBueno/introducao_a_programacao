13c34275ab6c9e5d385089f31f37a7ab9cde47ae
miqueias <miqueiasricardobueno@gmail.com>
2026-07-20 11:50:32 -0300

    Conversor de temperatura °C em °F.

--- capitulo3/exercicio_3_13.py
+++ capitulo3/exercicio_3_13.py
@@ -2,3 +2,8 @@
 #      9 × C
 #  F = ----- + 32
 #        5
+
+temp_celsius = round(float(input("Insira a temperatura em graus célsius: ")),1)
+temp_fahrenheit = (9 * temp_celsius)/ 5 + 32
+print(f"A temperatura {temp_celsius}°C")
+print(f"Em Fahrenheit é {temp_fahrenheit:.1f}°F")
