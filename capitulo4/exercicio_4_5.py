d782bbea084d9bd1f05719ea51e2df431f2be0a6
miqueias <miqueiasricardobueno@gmail.com>
2026-07-27 15:11:57 -0300

    exercícios do livro

---
+++ capitulo4/exercicio_4_5.py
@@ -0,0 +1,6 @@
+# Programa 4.5: Carro novo ou velho, dependendo da idade com else
+idade = int(input("Insira a idade do seu carro: "))
+if idade <= 3:
+    print("Seu carro é novo!")
+else:
+    print("Seu carro é velho!")
---
+++ capitulo4/exercicio_4_6.py
@@ -0,0 +1,5 @@
+# Programa 4.6: Lê dois valores e imprime qual é o maior com else
+
+valor_a, valor_b = int(input("Insira um valor: ")), int(input("Insira outro valor:"))
+if valor_a > valor_b: print("O primeiro valor é maior.")
+else: print("O segundo valor é maior.")
