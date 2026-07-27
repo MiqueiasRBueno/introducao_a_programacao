deacab2f69dd73f7c77c34965696fe359bad85a0
miqueias <miqueiasricardobueno@gmail.com>
2026-07-27 17:06:54 -0300

    Reescrevendo código com else.

--- capitulo4/exercicio_4_8.py
+++ capitulo4/exercicio_4_8.py
@@ -1,6 +1,5 @@
 # Reescreva o Programa 4.4 e calcule a conta da operadora Tchau usando else.
 # Cálculo da mensalidade de um plano de celular da operadora Tchau
-from capitulo4.programa_4_4 import minutos_consumidos
 
 plano = str(input("Insira seu plano: "))
 preco = minutos_no_plano = extra = 0
@@ -11,8 +10,15 @@ if plano == "falopouco":
 if plano == "falomuito":
     minutos_no_plano = 500
     extra = 0.15
-    plano = 99
+    preco = 99
 if plano == "falopouco" or plano == "falomuito":
     minutos_consumidos = int(input("Insira os minutos consumidos: "))
-    print("Você vai pagar:")
+    print("      Você vai pagar:")
     print(f"Preço do plano: R$ {preco:7.2f}")
+    suplemento = 0
+    if minutos_consumidos >= minutos_no_plano:
+        suplemento = (minutos_consumidos - minutos_no_plano) * extra
+        print(f"Minutos extras  R$ {suplemento:7.2f}")
+        print(f"Total           R$ {preco + suplemento:7.2f}")
+else:
+    print("Plano desconhecido!")
