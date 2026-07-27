a20f46da9b403898ce8b3a5f2924ccdeb5c91efb
miqueias <miqueiasricardobueno@gmail.com>
2026-07-27 16:09:43 -0300

    Exercício de demonstração de códigos diferentes, mesmo resultado.

---
+++ capitulo4/exercicio_4_5.py
@@ -0,0 +1,11 @@
+# Execute o Programa 4.5 e experimente alguns valores. Verifique se os resultados foram os mesmos do Programa 4.2.
+
+idade = int(input("Insira a idade do seu carro: "))
+if idade <= 3:
+    print("Seu carro é novo!")
+else:
+    print("Seu carro é velho!")
+if idade <= 3:
+    print("Seu carro é novo")
+if idade > 3:
+    print("Seu carro é velho")
