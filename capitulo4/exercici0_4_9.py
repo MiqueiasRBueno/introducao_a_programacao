f1ecd2f285c4c3311b26e2470cb5c239b1c3a698
miqueias <miqueiasricardobueno@gmail.com>
2026-07-28 15:37:31 -0300

    Praticar a técnica de rastreamento

---
+++ capitulo4/exercici0_4_9.py
@@ -0,0 +1,6 @@
+# Rastreie o Programa 4.8. Compare seu resultado ao apresentado na Tabela 4.2
+
+# O exercício consiste em rastrear o programa da listagem 4.8.
+# O resultado deve ser o mesmo do apresentado na tabela 4.2.
+# A técnica de rastreamento é apresentada na página 110,
+# seção 3.6 Rastreamento.
