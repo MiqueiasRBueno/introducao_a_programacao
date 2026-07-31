e1b6c2fd1736a9fcac7fac79c73348c49ead8f4e
miqueias <miqueiasricardobueno@gmail.com>
2026-07-31 09:43:02 -0300

    Correção de exercćios com if-elif-else

---
+++ capitulo4/exercicio_4_16.py
@@ -0,0 +1,9 @@
+# Corrija o programa a seguir:
+
+média = int(input("Digite sua média:"))
+if média <= 4:
+    print("Infelizmente você reprovou")
+elif média < 7:
+    print("Você ficou de recuperação")
+else:
+    print("Você passou de ano")
