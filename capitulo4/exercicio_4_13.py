5dffdeed724ddfeecac10f0bb5387950885e2359
miqueias <miqueiasricardobueno@gmail.com>
2026-07-31 09:13:08 -0300

    invertendo programa

---
+++ capitulo4/exercicio_4_13.py
@@ -0,0 +1,10 @@
+# No programa a seguir, inverta as linhas do if e else, negando a condição. 
+# Adicione as linhas necessárias para fazê-lo funcionar em Python.
+# if a > b:
+#    print("a é maior que b")
+#else:
+#    print("b é maior que a")
+
+a, b = map(int, input().split())
+if a <= b: print(" a é maior que b")
+else: print("b é maior que a")
