8fe8415489754ecf43b726529e9c6f4c0003a760
miqueias <miqueiasricardobueno@gmail.com>
2026-07-31 09:22:00 -0300

    Reescrevendo programa com if-elif-else

--- capitulo4/exercicio_4_14.py
+++ capitulo4/exercicio_4_14.py
@@ -3,5 +3,5 @@
 
 a = int(input("Insira um número inteiro: "))
 if a < 10: print("a é menor que 10.")
-elif a >= 10: print("a é maior que 10 e menor que 20")
-elif a >= 20: print("a é maior que 20.")
+elif a < 20: print("a é maior que 10 e menor que 20")
+else: print("a é maior que 20.")
---
+++ capitulo4/exercicio_4_15.py
@@ -0,0 +1,9 @@
+# Reescreva o programa a seguir com if-elif-else.
+#hora = int(input("Digite a hora atual:"))
+# if hora < 12:
+#     print("Bom dia!")
+# if hora >=12 and hora <=18:
+#     print("Boa tarde!")
+# if hora >=18:
+#      print("Boa noite!")
+
