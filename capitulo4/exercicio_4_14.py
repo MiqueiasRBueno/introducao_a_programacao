6bd4b3518e089df8b5abdd03c8129ac0f93640dc
miqueias <miqueiasricardobueno@gmail.com>
2026-07-31 09:18:08 -0300

    Reescrevendo um programa com if-elif-else

---
+++ capitulo4/exercicio_4_14.py
@@ -0,0 +1,7 @@
+# Reescreva o programa a seguir com if-elif-else. 
+# Adicione as linhas necessárias para fazê-lo funcionar em Python.
+
+a = int(input("Insira um número inteiro: "))
+if a < 10: print("a é menor que 10.")
+elif a >= 10: print("a é maior que 10 e menor que 20")
+elif a >= 20: print("a é maior que 20.")
