77c3df9c0c79adb0a401d2be3228895fbbf9f1e5
miqueias <miqueiasricardobueno@gmail.com>
2026-08-06 15:50:47 -0300

    Exercício de repetição com whille, Tabuada  usando adção ou subtração.

---
+++ capitulo5/exercicio_5_8.py
@@ -0,0 +1,13 @@
+# Escreva um programa que leia dois números. 
+# Imprima o resultado da multiplicação do primeiro pelo segundo.
+# Utilize apenas os operadores de soma e subtração para calcular o resultado.
+# Lembre-se de que podemos entender a multiplicação de dois números como somas sucessivas de um deles.
+# Assim, 4 × 5 = 5 + 5 + 5 + 5 = 4 + 4 + 4 + 4 + 4.
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+x = resultado = 0
+while x < num2:
+    resultado += num1
+    x += 1  
+print(f"Resultado = {resultado}")
